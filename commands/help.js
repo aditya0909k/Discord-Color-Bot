05b68b3484153b5eab6eb5b0712d348fe0e0d8e7
aditya0909k <85547196+aditya0909k@users.noreply.github.com>
2021-07-20 17:33:03 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,14 +7,12 @@ module.exports = {
         const helpEmbed = new Discord.MessageEmbed()
             .setColor(0x5865f2)
             .setAuthor('Made by cas#7139', 'https://64.media.tumblr.com/14127e159a2599cefb88b9ba951f1e41/tumblr_oo7pt0NdRL1ugye8so4_250.png')
-          //  .setThumbnail('https://d31sxl6qgne2yj.cloudfront.net/wordpress/wp-content/uploads/20190111101501/LSD-Colors.jpg')
             .addFields(
-                { name: '.color', value: 'The .color command accepts multiple variations of hex/rgb code values.' },
-                { name: 'HEX', value: 'The starting "#" is not required; capitalization does not matter.'},
-                { name: 'RGB', value: 'The starting "rgb" is not required, neither are the parantheses.'},
+                {name: '.color', value: 'The .color command accepts multiple variations of hex/rgb code values.' },
+                {name: 'HEX', value: 'The starting "#" is not required; capitalization does not matter.'},
+                {name: 'RGB', value: 'The starting "rgb" is not required, neither are the parantheses.'},
                 {name: 'The following work for HEX:', value: '.color #5865f2\n.color 5865f2\n.color 5865F2', inline: true},
                 {name: 'The following work for RGB:', value: '.color rgb(88, 101, 242)\n.color (88, 101, 242)\n.color 88, 101, 242', inline: true},
-
             )
         message.channel.send(helpEmbed);
     }
