52274ea09953fb1d1939ceae1e4cde5dd874d3a0
aditya0909k <85547196+aditya0909k@users.noreply.github.com>
2021-07-20 16:10:04 -0500

    Create bot.js
    At [TOKEN], the Discord bot's token would be inserted here. It is removed here to prevent others from accessing my bot.

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.';
+
+client.once('ready', () => { 
+    console.log("System online");
+});
+
+const fs = require('fs');
+client.commands = new Discord.Collection(); 
+const commandFiles = fs.readdirSync('./src/commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) 
+        return;
+
+    const args = message.content.slice(prefix.length).split(/ +/g); 
+    const command = args.shift().toLowerCase(); 
+
+    if (command === 'ping') 
+        client.commands.get('ping').execute(message, args); 
+    if (command === 'color')
+        client.commands.get('sendColor').execute(message, args);
+    if (command === 'help')
+        client.commands.get('help').execute(message, args);
+});
+
+client.login('[TOKEN]');
